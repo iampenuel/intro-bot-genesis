e611052bc52e406bc4ece9999ef22bd89a379af6
admin <admin@Penuels-MacBook-Pro.local>
2025-04-08 15:00:35 -0400

    Initial commit: Intro Bot Genesis project

---
+++ intro_bot.py
@@ -0,0 +1,14 @@
+# INTRO BOT: The Genesis Project
+first_name = input("What is your first name?: ")
+last_name = input("What is your last name?: ")
+full_name = first_name + " " + last_name
+print("Hey " + full_name + " nice to meet you!")
+print("You're now in the system! You can't go back now MWAHAHAHA!!!")
+# age in five years
+age = int(input("How old are you?: "))
+future_age = age + 5
+print("You will be " + str(future_age) + " in five years!")
+# favorite song
+best_song = input("What is your favorite song?: ")
+print("dangggg I don't know anyone who still listens to " + best_song + " uh! lameee")
+print("Just kidding... " + best_song + " is pretty cool!")
